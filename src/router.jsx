26ce4ef3a7d7b99c03b92a412a967d5200f548a8
sseul <hanbat107@naver.com>
2024-10-14 14:48:03 +0900

    feat: 공통 레이아웃 생성 및 Header 컴포넌트 이동

--- src/router.jsx
+++ src/router.jsx
@@ -1,9 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
-import App from "./App";
+import Layout from "./components/Layout/Layout";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: <Layout />,
+    children: [
+      // {
+      //   path: "/",
+      //   element: < />,
+      // },
+    ],
   },
 ]);
