9f2426c09c3443aa8c6c500c6bbf9a1ee9bf385a
sseul <hanbat107@naver.com>
2024-10-14 14:53:27 +0900

    feat: SearchPage 컴포넌트 추가 및 /search 경로 설정

--- src/router.jsx
+++ src/router.jsx
@@ -1,15 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
+import Search from "./pages/Search/SearchPage";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
     children: [
-      // {
-      //   path: "/",
-      //   element: < />,
-      // },
+      {
+        path: "/search",
+        element: <Search />,
+      },
     ],
   },
 ]);
